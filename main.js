3ad2a95307a8b92e57a3d31dcc8b19b8a78b8dd3
FatemehEi <fatemehebrahimi1998@yahoo.com>
2022-04-14 20:58:51 +0430

    complete weather app

---
+++ main.js
@@ -0,0 +1,52 @@
+const api = {
+    key: "c880974411ae39f77bbf7ff370baaeb8",
+    baseurl: "https://api.openweathermap.org/data/2.5/"
+}
+
+const searchbox = document.querySelector('.search-box');
+searchbox.addEventListener('keypress', setQuery);
+
+function setQuery(evn){
+    if(evn.keyCode == 13){
+        getResult(searchbox.value);
+        console.log(searchbox.value);
+    }
+}
+
+function getResult(query) {
+    fetch(`${api.baseurl}weather?q=${query}&units=metric&APPID=${api.key}`)
+    .then(weather => {
+        return weather.json();
+    }).then(displayResult);
+}
+
+function displayResult(weather) {
+    console.log(weather);
+    let city = document.querySelector('.location .city');
+    city.innerText = `${weather.name}, ${weather.sys.country}`;
+
+    let now = new Date()
+    let date = document.querySelector('.location .date');
+    date.innerText = dateBuilder(now);
+
+    let temp = document.querySelector('.current .temp');
+    temp.innerHTML = `${Math.round(weather.main.temp)}<span>°c</span>`;
+
+    let weather_el = document.querySelector('.current .weather');
+    weather_el.innerText = weather.weather[0].main;
+
+    let hilow = document.querySelector('.hi-low');
+    hilow.innerText = `${weather.main.temp_min}°c / ${weather.main.temp_max}°c`
+}
+
+function dateBuilder(d) {
+    console.log(d);
+    const months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+    const days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+
+    let day = days[d.getDay()];
+    let month = months[d.getMonth()];
+    let date = d.getDate();
+    let year = d.getFullYear();
+    return `${day} ${date} ${month} ${year}`;
+}
